bc332b0edb9bb22584684f3e62db183883771060
meKyleh <kyle.a.holt@gmail.com>
2016-11-20 15:58:28 -0500

    first commit

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+    [SerializeField]
+    private Transform target;
+    [SerializeField]
+    private Vector3 offsetPosition;
+
+    public void Update()
+    {
+        if (target == null)
+        {
+            Debug.LogWarning("Missing target ref !", this);
+            return;
+        }
+
+        // compute position
+        transform.position = target.position + offsetPosition;
+        
+
+        // compute rotation
+         //transform.rotation = offsetRotation;
+    }
+}
