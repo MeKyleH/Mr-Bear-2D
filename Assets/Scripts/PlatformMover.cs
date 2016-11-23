4d3242dad29cf52a9f34cf7bac0079c90672295d
meKyleh <kyle.a.holt@gmail.com>
2016-11-22 21:00:33 -0500

    Adding movable platforms

---
+++ Assets/Scripts/PlatformMover.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class PlatformMover : MonoBehaviour
+{
+    public enum FollowType
+    {
+        MoveTowards,
+        Lerp
+    }
+
+    [SerializeField]
+    public float speed = 1;
+
+    public FollowType type = FollowType.MoveTowards;
+    public PlatformPath path;
+    public float maxDistanceToGoal = 0.1f;
+
+    private IEnumerator<Transform> _currentPoint;
+
+    public void Start()
+    {
+        if (path == null)
+        {
+            Debug.LogError("Path cannot be null ", gameObject);
+            return;
+        }
+
+        _currentPoint = path.GetPathsEnumerator();
+        _currentPoint.MoveNext();
+
+        if (_currentPoint.Current == null)
+        {
+            return;
+        }
+
+        transform.position = _currentPoint.Current.position;
+    }
+        
+    public void Update()
+    {
+        if (_currentPoint == null || _currentPoint.Current == null)
+        {
+            return;
+        }
+
+        if (type == FollowType.MoveTowards)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, _currentPoint.Current.position, Time.deltaTime * speed);
+        }
+        else if (type == FollowType.Lerp)
+        {
+            transform.position = Vector3.Lerp(transform.position, _currentPoint.Current.position, Time.deltaTime * speed);
+        }
+
+        var distanceSquared = (transform.position -
+        _currentPoint.Current.position).sqrMagnitude; if (distanceSquared <
+        maxDistanceToGoal * maxDistanceToGoal)
+        {
+            _currentPoint.MoveNext();
+        }
+    }   
+}
---
+++ Assets/Scripts/PlatformPath.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Collections;
+
+public class PlatformPath : MonoBehaviour
+{
+    public Transform[] points;
+
+    public IEnumerator<Transform> GetPathsEnumerator()
+    {
+        if (points.Length < 1)
+        {
+            yield break;
+        }
+
+        var direction = 1;
+        var index = 0;
+        while (true)
+        {
+            yield return points[index];
+
+            if (points.Length == 1)
+            {
+                continue;
+            }
+
+            if (index <= 0)
+            {
+                direction = 1;
+            }
+            else if (index >= points.Length - 1)
+            {
+                direction = -1;
+            }
+
+            index = index + direction;
+        }
+    }
+
+    public void OnDrawGizmos()
+    {
+        if (points == null || points.Length < 2)
+        {
+            return;
+        }
+
+        for (var i = 1; i < points.Length; i++)
+        {
+            Gizmos.DrawLine(points[i - 1].position, points[i].position);
+        }
+    }
+}
