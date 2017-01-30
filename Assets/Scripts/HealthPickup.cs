289fe8e4ea980cf76316456096793da406bf369b
meKyleh <kyle.a.holt@gmail.com>
2017-01-29 22:03:53 -0600

    added health pickup

---
+++ Assets/Scripts/HealthPickup.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthPickup : MonoBehaviour {
+    [SerializeField]
+    private int healAmount = 25;
+
+    private PlayerHealthManager playerHealth;
+
+	// Use this for initialization
+	void Start () {
+        playerHealth = GameObject.FindObjectOfType<PlayerHealthManager>();
+        if(!playerHealth)
+        {
+            Debug.Log(name + " couldn't find PlayerHealthManager");
+        }
+	}
+
+    void FixedUpdate()
+    {
+        gameObject.transform.Rotate(new Vector3(0f, 1f, 0f));
+    }
+
+    private void OnTriggerEnter(Collider collider)
+    {
+        if(collider.tag == "Player")
+        {
+            playerHealth.HealPlayer(healAmount);
+           Destroy(gameObject);
+        }
+    }
+}
